53bcbca188e3257f5b823526d767d587f708334c
DESKTOP-QSHRR9D\Mitch <mitch.ronco@gmail.com>
2016-09-18 19:59:59 -0400

    refactored approach

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,119 +1,58 @@
-<<<<<<< HEAD
-# 1.Merges the training and the test sets to create one data set.
-# 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3.Uses descriptive activity names to name the ActsObject in the data set
-# 4.Appropriately labels the data set with descriptive variable names. 
-# 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+setwd("D://temp/TidyHAR/data/")
 
-#Ensure we have proper libraries:
-library(data.table)
+###Read Test Data
+testDataSet = read.csv("test/X_test.txt", sep="", header=FALSE)
+testDataSet[,562] = read.csv("test/Y_test.txt", sep="", header=FALSE)
+testDataSet[,563] = read.csv("test/subject_test.txt", sep="", header=FALSE)
 
-# 1.Merges the training and the test sets to create one data set.
-
-#      start by downloading the files
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-
-download.file(fileUrl, destfile = "Dataset.zip")
-
-unzip("Dataset.zip")
-
-testSet <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
-testSetActivities <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
-testSetSub <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
-trainSet <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
-trainSetActivities <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
-trainSetSub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+###Read trainingDataSet Data
+trainingDataSet = read.csv("train/X_train.txt", sep="", header=FALSE)
+trainingDataSet[,562] = read.csv("train/Y_train.txt", sep="", header=FALSE)
+trainingDataSet[,563] = read.csv("train/subject_train.txt", sep="", header=FALSE)
 
 
-# 3.Uses descriptive activity names to name the ActsObject in the data set
-ActsObject <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,colClasses="character")
-testSetActivities$V1 <- factor(testSetActivities$V1,levels=ActsObject$V1,labels=ActsObject$V2)
-trainSetActivities$V1 <- factor(trainSetActivities$V1,levels=ActsObject$V1,labels=ActsObject$V2)
+### Get the list of labels
+activityLabels = read.csv("activity_labels.txt", sep="", header=FALSE)
+
+### Adjust attributes
+attributes = read.csv("features.txt", sep="", header=FALSE)
+attributes[,2] = gsub('-mean', 'Mean', attributes[,2])
+attributes[,2] = gsub('-std', 'Std', attributes[,2])
+attributes[,2] = gsub('[-()]', '', attributes[,2])
+
+# Merge both data sets together
+fullData = rbind(trainingDataSet, testDataSet)
 
 
-# 4.Appropriately labels the data set with descriptive variable names. 
-features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE,colClasses="character")
-colnames(testSet)<-features$V2
-colnames(trainSet)<-features$V2
-colnames(testSetActivities)<-c("Activity")
-colnames(trainSetActivities)<-c("Activity")
-colnames(testSetSub)<-c("Subject")
-colnames(trainSetSub)<-c("Subject")
+# Limit the data to the mean and the standard deviation.
+desiredColumns <- grep(".*Mean.*|.*Std.*", attributes[,2])
 
-#1.  Merges the training and the test sets to create one data set.
-testSet<-cbind(testSet,testSetActivities)
-testSet<-cbind(testSet,testSetSub)
-trainSet<-cbind(trainSet,trainSetActivities)
-trainSet<-cbind(trainSet,trainSetSub)
-aggregateData<-rbind(testSet,trainSet)
+# First reduce the attributes table to what we want
+attributes <- attributes[desiredColumns,]
 
-# 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
-aggregateDataMean<-sapply(aggregateData,mean,na.rm=TRUE)
-aggregateDataStandardDeviation<-sapply(aggregateData,sd,na.rm=TRUE)
+# Add columns for subject and activity
+desiredColumns <- c(desiredColumns, 562, 563)
 
+# And remove the unwanted columns from fullData
+fullData <- fullData[,desiredColumns]
 
+# Add the column names (attributes) to fullData
+colnames(fullData) <- c(attributes$V2, "Activity", "Subject")
+colnames(fullData) <- tolower(colnames(fullData))
 
-# 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-DT <- data.table(aggregateData)
-tidy<-DT[,lapply(.SD,mean),by="Activity,Subject"]
-write.table(tidy,file="tidy.csv",sep=",",col.names = NA)
-=======
-# 1.Merges the training and the test sets to create one data set.
-# 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3.Uses descriptive activity names to name the ActsObject in the data set
-# 4.Appropriately labels the data set with descriptive variable names. 
-# 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+currentActivity = 1
+for (currentActivityLabel in activityLabels$V2) {
+  fullData$activity <- gsub(currentActivity, currentActivityLabel, fullData$activity)
+  currentActivity <- currentActivity + 1
+}
 
-#Ensure we have proper libraries:
-library(data.table)
+fullData$activity <- as.factor(fullData$activity)
+fullData$subject <- as.factor(fullData$subject)
 
-# 1.Merges the training and the test sets to create one data set.
-
-#      start by downloading the files
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-
-download.file(fileUrl, destfile = "Dataset.zip")
-
-unzip("Dataset.zip")
-
-testSet <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
-testSetActivities <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
-testSetSub <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
-trainSet <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
-trainSetActivities <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
-trainSetSub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+tidy = aggregate(fullData, by=list(activity = fullData$activity, subject=fullData$subject), mean)
+# The mean of these two columns have no meaning.
+tidy[,90] = NULL
+tidy[,89] = NULL
+write.table(tidy, "tidy.txt", sep="\t")
 
 
-# 3.Uses descriptive activity names to name the ActsObject in the data set
-ActsObject <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,colClasses="character")
-testSetActivities$V1 <- factor(testSetActivities$V1,levels=ActsObject$V1,labels=ActsObject$V2)
-trainSetActivities$V1 <- factor(trainSetActivities$V1,levels=ActsObject$V1,labels=ActsObject$V2)
-
-
-# 4.Appropriately labels the data set with descriptive variable names. 
-features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE,colClasses="character")
-colnames(testSet)<-features$V2
-colnames(trainSet)<-features$V2
-colnames(testSetActivities)<-c("Activity")
-colnames(trainSetActivities)<-c("Activity")
-colnames(testSetSub)<-c("Subject")
-colnames(trainSetSub)<-c("Subject")
-
-#1.  Merges the training and the test sets to create one data set.
-testSet<-cbind(testSet,testSetActivities)
-testSet<-cbind(testSet,testSetSub)
-trainSet<-cbind(trainSet,trainSetActivities)
-trainSet<-cbind(trainSet,trainSetSub)
-aggregateData<-rbind(testSet,trainSet)
-
-# 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
-aggregateDataMean<-sapply(aggregateData,mean,na.rm=TRUE)
-aggregateDataStandardDeviation<-sapply(aggregateData,sd,na.rm=TRUE)
-
-
-
-# 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-DT <- data.table(aggregateData)
-tidy<-DT[,lapply(.SD,mean),by="Activity,Subject"]
-write.table(tidy,file="tidy.csv",sep=",",col.names = NA)
->>>>>>> origin/master
